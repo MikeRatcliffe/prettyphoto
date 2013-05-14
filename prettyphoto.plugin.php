c687ce21fbb52e045f6cfc18e45129d3dff51132
mikelietz <codger+github@gmail.com>
2013-05-14 14:40:43 +0000

    Add jquery to the header if the theme doesn't already do it.

--- prettyphoto.plugin.php
+++ prettyphoto.plugin.php
@@ -10,11 +10,12 @@ class PrettyPhoto extends Plugin
 	{
 		Plugins::act('add_prettyphoto_template');
 	}
-	
+
 	public function action_add_prettyphoto_template()
 	{
-		Stack::add('template_stylesheet', array($this->get_url() . '/css/prettyPhoto.css', 'screen'));
-		Stack::add('template_header_javascript', $this->get_url() . '/js/jquery.prettyPhoto.js', 'prettyphoto', 'jquery');
+		Stack::add( 'template_header_javascript', Site::get_url('vendor') . '/jquery.js', 'jquery' );
+		Stack::add( 'template_stylesheet', array($this->get_url() . '/css/prettyPhoto.css', 'screen' ) );
+		Stack::add( 'template_header_javascript', $this->get_url() . '/js/jquery.prettyPhoto.js', 'prettyphoto', 'jquery' );
 	}
 
 	public function action_add_prettyphoto_admin()
