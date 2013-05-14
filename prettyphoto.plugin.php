62c75709d3043cf4b0493dade574d567a7a7dd6f
Konzertheld <github@konzertheld.de>
2013-05-14 18:24:22 +0200

    Add option to hide the social buttons

--- prettyphoto.plugin.php
+++ prettyphoto.plugin.php
@@ -6,6 +6,14 @@
  */
 class PrettyPhoto extends Plugin
 {
+	public function configure()
+	{
+		$form = new FormUI(__CLASS__);
+		$form->append('checkbox', 'hide_social', __CLASS__ . 'hide_social', _t('Hide social area in fullsize image view', __CLASS__));
+		$form->append('submit', 'submit', _t('Save'));
+		return $form;
+	}
+	
 	public function action_template_header($theme)
 	{
 		Plugins::act('add_prettyphoto_template');
@@ -13,11 +21,13 @@ class PrettyPhoto extends Plugin
 
 	public function action_add_prettyphoto_template()
 	{
+		$social_string = (Options::get(__CLASS__ . 'hide_social', false)) ? "{social_tools:false}" : "";
+
 		Stack::add( 'template_header_javascript', Site::get_url('vendor') . '/jquery.js', 'jquery' );
 		Stack::add( 'template_stylesheet', array($this->get_url() . '/css/prettyPhoto.css', 'screen' ) );
 		Stack::add( 'template_header_javascript', $this->get_url() . '/js/jquery.prettyPhoto.js', 'prettyphoto', 'jquery' );
-
-		$init_string = "$(document).ready(function(){ $(\"a[rel^='prettyPhoto']\").prettyPhoto();});";
+		
+		$init_string = "$(document).ready(function(){ $(\"a[rel^='prettyPhoto']\").prettyPhoto(" . $social_string . ");});";
 		Stack::add( 'template_header_javascript', $init_string, 'prettyphoto_init', 'prettyphoto' );
 	}
 
